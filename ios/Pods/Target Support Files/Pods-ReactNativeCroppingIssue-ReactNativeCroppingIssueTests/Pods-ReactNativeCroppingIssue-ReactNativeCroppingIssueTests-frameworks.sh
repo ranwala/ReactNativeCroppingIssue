f6f8c2e730e07cf4417e76bf9b39bf8a8d5038b8
Chandu Ranwala <54301535+ranwala@users.noreply.github.com>
2024-04-17 12:17:33 +0200

    add new script

--- ios/Pods/Target Support Files/Pods-ReactNativeCroppingIssue-ReactNativeCroppingIssueTests/Pods-ReactNativeCroppingIssue-ReactNativeCroppingIssueTests-frameworks.sh
+++ ios/Pods/Target Support Files/Pods-ReactNativeCroppingIssue-ReactNativeCroppingIssueTests/Pods-ReactNativeCroppingIssue-ReactNativeCroppingIssueTests-frameworks.sh
@@ -176,9 +176,6 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/Flipper-DoubleConversion/double-conversion.framework"
-  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/Flipper-Glog/glog.framework"
-  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/OpenSSL-Universal/OpenSSL.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/ScanbotSDK/ALL/ScanbotSDK.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/ScanbotSDKNativeWrapper/ALL/ScanbotSDKNativeWrapper.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/hermes-engine/Pre-built/hermes.framework"
